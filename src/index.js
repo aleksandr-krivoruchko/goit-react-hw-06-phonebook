9573a9f1884c70c1ac535cbb4c96abb3d126bdd9
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-04-30 20:40:05 +0300

    added redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './app/store';
+import { Provider } from 'react-redux';
+
 import { App } from 'components/App/App.jsx';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+	  <Provider store={store}>
+		<App />
+		</Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
